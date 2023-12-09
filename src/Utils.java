7454332fd4d2ada1dc36e5ddf24842e741b761df
L <L>
2023-12-09 02:15:25 +0100

    Add javadocs

--- src/Utils.java
+++ src/Utils.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 
+/**
+ * IO utils service.
+ */
 public class Utils {
-    
+
     /**
-     * 
      * @param question - question to ask user
-     * @return - user input as a double. If not a double it will prompt the user again
+     * @return - input as a double. If not a double it will prompt the user again
      */
     public static double askForDouble(String question) {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            try {
-                System.out.println(question);
-                return Double.parseDouble(scanner.nextLine());
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid input.");
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                try {
+                    System.out.println(question);
+                    return Double.parseDouble(scanner.nextLine());
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input.");
+                }
             }
         }
     }
@@ -25,8 +28,9 @@ public class Utils {
      * @return - user input. If not n the program will restart
      */
     public static String askForString(String question) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(question);
-        return scanner.nextLine();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println(question);
+            return scanner.nextLine();
+        }
     }
 }
