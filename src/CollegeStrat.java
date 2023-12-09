10ce1d15c41bb0f8681c39f1e8542a4e0740f710
Lucas1774 <lucas.115@hotmail.com>
2023-12-09 02:15:25 +0100

    Add javadocs

--- src/CollegeStrat.java
+++ src/CollegeStrat.java
@@ -1,9 +1,13 @@
+/**
+ * Console program to help you approach a subject or a test of it.
+ */
 public class CollegeStrat {
-    
+
     public static void main(String[] args) {
         String again = "y";
         while (!again.equals("n")) {
-            int ans = (int) Utils.askForDouble("What program do you want to run?\n" + "1 - Subject calculator.\n" + "2 - Test calculator.");
+            int ans = (int) Utils.askForDouble(
+                    "What program do you want to run?\n" + "1 - Subject calculator.\n" + "2 - Test calculator.");
             switch (ans) {
                 case 1:
                     SubjectStrat.run();
--- src/SubjectStrat.java
+++ src/SubjectStrat.java
@@ -1,5 +1,14 @@
+/**
+ * Service to run a subject optimizer.
+ */
 public class SubjectStrat {
 
+    /**
+     * Run the Subject program, which returns two scenarios to pass given the
+     * exam weight and the hard minimum exam score. The first one assumes you get
+     * the maximum score in the assignments that will help you pass. THe second
+     * assumes you get the bare minimum to still be able to.
+     */
     public static void run() {
         double assignmentScore = 10;
         double examWeight = -1;
@@ -11,12 +20,15 @@ public class SubjectStrat {
         }
         double minExam = Utils.askForDouble("What is the minimum exam score to pass?");
         double needed = (assignmentScore * examWeight - 5) / examWeight;
-        
+
         if (needed < minExam) {
             needed = minExam;
             assignmentScore = (5 - examWeight * needed) / (1 - examWeight);
         }
-        System.out.println(String.format("%.2f", assignmentScore) + " in assignments and " + String.format("%.2f", needed) + " in exam for a score of " + String.format("%.2f", (needed * examWeight + (1 - examWeight) * assignmentScore)));
-        System.out.println("0 in assignments and " + String.format("%.2f", (5 / examWeight)) + " in exam for a score of 5");
+        System.out.println(String.format("%.2f", assignmentScore) + " in assignments and "
+                + String.format("%.2f", needed) + " in exam for a score of "
+                + String.format("%.2f", (needed * examWeight + (1 - examWeight) * assignmentScore)));
+        System.out.println(
+                "0 in assignments and " + String.format("%.2f", (5 / examWeight)) + " in exam for a score of 5");
     }
 }
